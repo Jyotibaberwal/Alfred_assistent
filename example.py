0f116389ce122753ac67ebe61751a33d98befdc8
Jyoti baberwal <jyotibaberwal1112001@gmail.com>
2024-12-18 17:33:17 +0530

    updated_some_files

--- example.py
+++
@@ -1,7 +0,0 @@
-import google.generativeai as genai
-
-genai.configure(api_key="AIzaSyAdTjdjdfnAhhI4MxxRDw1jJ1oRMl8v-d0")
-model = genai.GenerativeModel("gemini-1.5-flash")
-response = model.generate_content("Explain how AI works")
-print(response.text)
-
